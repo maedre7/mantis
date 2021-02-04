7871cba804255ec97429b307cd769ebc8543678e
krishna104 <goyalkrishna104@gmail.com>
2021-02-04 17:20:22 +0530

    Initial commit

---
+++ src/components/home.js
@@ -0,0 +1,24 @@
+import React from "react";
+
+class Home extends React.Component{
+
+  constructor(props){
+    super(props);
+  }
+
+  componentDidMount(){
+
+  }
+
+  render(){
+
+    return(
+      <div id="home-container">
+        <p>Hey there!</p>
+      </div>
+    );
+  }
+
+}
+
+export default Home;
